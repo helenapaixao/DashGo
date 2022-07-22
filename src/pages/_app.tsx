f0acbd291fae0c00c90638fa16e1a4e02a28cc0d
helenapaixao <hp.helenapaixao@gmail.com>
2022-07-22 12:20:50 -0300

    feat: add mirage and faker in project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SideBarDrawerProvider } from "../contexts/SideBarDrawerContext";
+import { makeServer } from "../services/mirage";
 
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+
+}
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
